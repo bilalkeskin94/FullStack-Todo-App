84c81c09efb69c672993123a4199427ec69d7570
bilal <bilalkeskin_94@hotmail.com>
2020-09-14 14:44:37 +0300

    init

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/FullStack-Todo-App");
 const app = express();
 
 app.listen(4000, () => console.log("Sunucu port 4000'de çalıştı"));
