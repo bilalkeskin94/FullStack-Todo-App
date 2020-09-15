d18937633f686768efdc617178776192e1aaf4f9
Bilal Keskin <bilalkeskin_94@hotmail.com>
2020-09-15 16:08:58 +0300

    Updates

---
+++ precache-manifest.30af2b744106f0346e8b5a8687cee64b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47670cacda692cc59fd9b607b689d4c",
+    "url": "/FullStack-Todo-App/index.html"
+  },
+  {
+    "revision": "ff971b80cbb49d613e1a",
+    "url": "/FullStack-Todo-App/static/css/main.6ecb0895.chunk.css"
+  },
+  {
+    "revision": "18e099d6045e1f9d2ad0",
+    "url": "/FullStack-Todo-App/static/js/2.7940d821.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FullStack-Todo-App/static/js/2.7940d821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff971b80cbb49d613e1a",
+    "url": "/FullStack-Todo-App/static/js/main.8b6cf136.chunk.js"
+  },
+  {
+    "revision": "f489b3dc9f4d23697d13",
+    "url": "/FullStack-Todo-App/static/js/runtime-main.97430e58.js"
+  }
+]);
